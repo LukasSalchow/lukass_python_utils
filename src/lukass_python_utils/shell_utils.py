db961ba44dc8fee5101bc00f13e9f5875209e35c
Lukas Salchow <lukas-michael.salchow@t-systems.com>
2024-11-05 14:38:49 +0100

    add shell_utils

---
+++ src/lukass_python_utils/shell_utils.py
@@ -0,0 +1,28 @@
+import subprocess
+from typing import Sequence
+
+TIMEOUT = 10
+
+
+def run_shell_command(command: Sequence[str], timeout=TIMEOUT) -> subprocess.CompletedProcess[bytes]:
+    """Wrapper for "subprocess.run" that adds the output in case of an error."""
+    try:
+        output = subprocess.run(  # noqa: S603
+            command,
+            capture_output=True,
+            timeout=timeout,
+        )
+    except subprocess.TimeoutExpired as e:
+        e.add_note("\noutput =\n" + e.output.decode())
+        raise
+    if output.returncode != 0:
+        message = "\n".join(
+            (
+                "returned error code: " + str(output.returncode),
+                output.stdout.decode("utf-8"),
+                output.stderr.decode("utf-8"),
+            )
+        )
+        raise RuntimeError(message)
+
+    return output
